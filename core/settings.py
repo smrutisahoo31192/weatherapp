1838533b19cca9d1696f9ea833a5475d111e0cb8
smruti <aekutetechnologies@gmail.com>
2023-10-05 18:32:47 +0530

    changes

--- core/settings.py
+++ core/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.postgresql_psycopg2",
-#         "NAME": "postgres",
-#         "USER": "postgres",
-#         "PASSWORD": "misspuja",
-#         "HOST": "database-1.ckvjyil5giyb.us-east-2.rds.amazonaws.com",
-#         "PORT": "5432",
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "misspuja",
+        "HOST": "database-2.ckvjyil5giyb.us-east-2.rds.amazonaws.com",
+        "PORT": "5432",
+    }
+}
 
 
 # Password validation
